c924aaeaf4dd2b902cb4fd29e68573f8e4fac2b7
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-22 16:59:59 +0000

    HeroBanner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -9,7 +9,7 @@ const HeroBanner = () => {
         <Typography color="#FF2625" fontWeight="600" fontSize="26px">
             Fitness Club
         </Typography>
-        <Typography fontWeight="700" sx={{ fontSize: { lg: '44px', xs: '40px' } }}>
+        <Typography fontWeight="700" mb="30px" mt="30px" sx={{ fontSize: { lg: '44px', xs: '40px' } }}>
           Sweat, Smile <br /> and Repeat
         </Typography>
         <Typography fontSize="22px" lineHeight="35px" mb={4}>
@@ -18,7 +18,7 @@ const HeroBanner = () => {
         <Typography fontWeight={600} color="#FF2625" fontSize="200px" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' } }}>
           Exercise
         </Typography>
-        <Button variant='contained' color='error' href='#exercises'>Explore Exercise</Button>
+        <Button variant='contained' color='error' href='#exercises' sx={{ backgroundColor: '#FF2625', padding: '13px' }} >Explore Exercise</Button>
         <img src={HeroBannerImage} alt='banner' className='hero-banner-img'/>
     </Box>
   )
