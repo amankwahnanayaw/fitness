da3c39d74127c3e4e7ab2d7bb28cf10e3d679e4e
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-23 19:10:10 +0000

    working on some util to fetches data from api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOption = {
     method: 'GET',
     params: {limit: '10'},
     headers: {
-      'X-RapidAPI-Key': '41bb903547mshb5840dbd36c9e80p190862jsn2929c20ce85c',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
