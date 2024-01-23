c45abab2edd4258d9bdc07b917223ea697d034da
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-23 19:03:28 +0000

    working on some util to fetches data from api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,15 @@
+export const exerciseOption = {
+    method: 'GET',
+    params: {limit: '10'},
+    headers: {
+      'X-RapidAPI-Key': '41bb903547mshb5840dbd36c9e80p190862jsn2929c20ce85c',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
-    
+
     return data;
 }
