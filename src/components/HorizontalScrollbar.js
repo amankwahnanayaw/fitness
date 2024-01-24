cf0c169c2a2ef8245a0ec7d6c6bd20a635e1f5f7
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-24 14:19:23 +0000

    added bodypart component

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Box } from '@mui/material';
 
+import BodyPart from './BodyPart';
+
 const HorizontalScrollbar = ({ data }) => {
   return (
     <div>
         {data.map((item) => (
              <Box key={item.id || item} itemID={ item.id || item } title={ item.id || item } m='0, 40px'>
-             { item }
+             <BodyPart item={item}/>
             </Box>
         )   
         )}
