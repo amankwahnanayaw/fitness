e0efdf1734213c0ec848587dae47c32079a596c3
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-24 14:25:48 +0000

    added bodypart component

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -8,7 +8,7 @@ const HorizontalScrollbar = ({ data }) => {
     <div>
         {data.map((item) => (
              <Box key={item.id || item} itemID={ item.id || item } title={ item.id || item } m='0, 40px'>
-             <BodyPart item={item}/>
+             <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
             </Box>
         )   
         )}
