3bdc5bffd84b554cc22524fc8889a14a10801f8a
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-26 12:41:50 +0000

    working on the exercises diplay

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Pagination } from '@mui/material/Pagination';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const Exercises = () => {
   return (
