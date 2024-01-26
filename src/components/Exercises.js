18b879fb4cfc09b8cd7e24159b4b6e6748e22514
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-26 14:17:20 +0000

    added exercise card

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,7 +6,9 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodypart }) => {
-  console.log(exercises);
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+
   return (
     <Box id="exercises" 
     sx={{ mt: {lg: '110px'} }}
@@ -27,7 +29,7 @@ const Exercises = ({ exercises, setExercises, bodypart }) => {
           color="standard"
           shape="rounded"
           defaultPage={1}
-          count={Math.ceil(exercises.lenght / 9)}
+          count={Math.ceil(exercises.lenght / exercisesPerPage)}
           page={currentPage}
           onChange={paginate}
           size="large"
