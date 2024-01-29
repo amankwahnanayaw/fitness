96f301e129e15fe7448c4b802034548fb573c65b
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-29 12:07:49 +0000

    added componennts for Exercise Detail

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -3,6 +3,9 @@ import {useParams} from 'react-router-dom';
 import {Box} from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import SimilarExercises from '../components/SimilarExercises';
+import ExerciseVideo from '../components/ExerciseVideo';
 
 const ExerciseDetail = () => {
   return (
