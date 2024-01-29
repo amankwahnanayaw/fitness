668d3981966b90030245918822ea557b44739728
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-29 10:20:16 +0000

    linking the exercises to the clickable icons

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { Pagination } from '@mui/material/Pagination';
+import { Pagination } from '@mui/material';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
-const Exercises = ({ exercises, setExercises, bodypart }) => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const exercisesPerPage = 9;
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
-  const currentExercises = exercise.slice(indexOfFirstExercise, indexOfLastExercise)
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
 
   const paginate = (e, value) => {
     setCurrentPage(value);
 
     window.scrollTo({ top:1800, behavior: 'smooth' })
   }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if(bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodypart/${bodyPart}', exerciseOptions, exerciseOptions);
+      }
+    }
+  })
 
   return (
     <Box id="exercises" 
