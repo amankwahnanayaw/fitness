16ef7a341da93aeade0697dea93425a82c1e7a85
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-30 08:52:51 +0000

    exercise detail problem

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -5,7 +5,7 @@ import {Box} from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import SimilarExercises from '../components/SimilarExercises';
-import ExerciseVideos from '../components/ExerciseVideo';
+import ExerciseVideos from '../components/ExerciseVideos';
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
@@ -26,7 +26,7 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail}/>
-      <ExerciseVideo />
+      <ExerciseVideos />
       <SimilarExercises />
     </Box>
   )
