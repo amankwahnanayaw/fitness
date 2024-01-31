d22e5e699b4a1f049d532d3d8a662aafd94d6024
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-31 09:50:17 +0000

    creating a loader

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { InfinitySpin } from 'react-loader-spinner';
+
+const loader = () => {
+  return (
+    <Stack direction='row' justifyContent='center' alignItems='center'>
+
+    </Stack>
+  )
+}
+
+export default loader
