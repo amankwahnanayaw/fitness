9b10387db92a46b8d4f07eb74fb349f5df8eb6ca
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-31 10:05:32 +0000

    creating a loader

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -33,7 +33,8 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
         {data.map((item) => (
              <Box key={item.id || item} itemID={ item.id || item } title={ item.id || item } m='0, 40px'>
-             {isBodyParts && <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />}
+             {isBodyParts ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+             : <ExerciseCard item={item} />}
             </Box>
         )   
         )}
