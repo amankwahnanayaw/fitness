5f03f535e5ecf4327e57e95a5b3ab9e327a98a3b
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-31 09:51:15 +0000

    creating a loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -5,7 +5,7 @@ import { InfinitySpin } from 'react-loader-spinner';
 const loader = () => {
   return (
     <Stack direction='row' justifyContent='center' alignItems='center' width='100%'>
-
+        <InfinitySpin />
     </Stack>
   )
 }
